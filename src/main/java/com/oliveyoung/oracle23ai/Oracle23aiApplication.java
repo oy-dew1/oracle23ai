ddac225882f8eb815f09814421bc69275bcdb6ed
DBA 이동주 <dongju.lee10@oliveyoung.co.kr>
2025-05-12 17:49:29 +0900

    Initial commit

---
+++ src/main/java/com/oliveyoung/oracle23ai/Oracle23aiApplication.java
@@ -0,0 +1,13 @@
+package com.oliveyoung.oracle23ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oracle23aiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oracle23aiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oliveyoung/oracle23ai/Oracle23aiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oliveyoung.oracle23ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oracle23aiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
